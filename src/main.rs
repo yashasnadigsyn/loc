4bf69e6c0d88f5b9b2ef3907a12629c3c5d3c9dd
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2023-12-25 08:39:56 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use comfy_table::Table;
 static IGNORE_DIRS: [&str; 3] = ["dev", "proc", "sys"]; 
 
 fn main() {
-    //let starting_path = "/home/".to_owned() + whoami::username().as_str(); //starting path for searching files and folders
+    let mut starting_path = "/";//starting path for searching files and folders
     let args: Vec<String> = env::args().collect();
     let mut files_matched: Vec<String> = Vec::new();
     let search_term = args.get(1);
@@ -17,15 +17,24 @@ fn main() {
         None => {println!("No name specified!\nAbort!"); exit(0)}, 
     }
 
+    if fname.contains("--help") {
+        println!("This is locate but written in rust.\n Usage: loc [filename] [args].\n Args: 1) --home-only - searches only in $HOME (false by default).\n 2) --exclude-dirs - searches only for files and not directories (false by default)");
+        exit(0);
+    }
+
     if args.contains(&("--exclude-dirs".to_string())) {
         exclude_dirs = true;
     }
+    if args.contains(&("--home-only".to_string())) {
+        starting_path = "/home";
+    }
 
-    get_fname(&fname, "/", &mut files_matched, exclude_dirs);
+    get_fname(&fname, starting_path, &mut files_matched, exclude_dirs);
     pprint(files_matched);
 }
 
 fn get_fname(fname: &String, dirname: &str, files_matched: &mut Vec<String>, exclude_dirs: bool) {
+    //println!("{}", &dirname);
     let paths = Path::new(dirname);
     let all_paths = paths.read_dir();
     // match all_paths {
